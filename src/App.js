f8b5dc7c1388569efe839d9ce527dca2d857df50
danghuutoan <danghuutoan@gmail.com>
2020-06-28 21:41:18 +0700

    - Adding basic routing for the page - Adding dummy page for testing purpose

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import Shop from "./components/Shop";
+import Navbar from "./components/common/Navbar";
+import Blog from "./components/Blog";
+import AboutUs from "./components/AboutUs";
+import Home from "./components/Home";
 function App() {
-	return <Shop />;
+	return (
+		<div>
+			<Navbar />
+			<div className="content">
+				<Switch>
+					<Route path="/shop" component={Shop} />
+					<Route path="/blog" component={Blog} />
+					<Route path="/about-us" component={AboutUs} />
+					<Route path="/" exact component={Home} />
+				</Switch>
+			</div>
+		</div>
+	);
 }
 
 export default App;
